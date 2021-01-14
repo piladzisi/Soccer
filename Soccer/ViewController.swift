c9d6a5cf8d571016f50c73f324e8de80bc374fca
piladzisi <piladzisi@gmail.com>
2021-01-15 00:35:37 +0200

    matchmaking

--- Soccer/ViewController.swift
+++ Soccer/ViewController.swift
@@ -14,9 +14,7 @@ class ViewController: UIViewController {
     
     var numberOfGames: Int = 0
     var numberOfTeams: Int = 0
-    
-    var hostTeams = [Int]()
-    var guestTeams = [Int]()
+    var teams = [Int]()
     
     @IBAction func didTapLetsPlay() {
         guard
@@ -24,74 +22,52 @@ class ViewController: UIViewController {
             let teams = numberOfTeamsTextField.text
         else { return }
         
-        numberOfGames = Int(games)!
-        numberOfTeams = Int(teams)!
+       numberOfGames = Int(games)!
+       numberOfTeams = Int(teams)!
         
-        hostTeams.removeAll()
-        guestTeams.removeAll()
-        
-        createArrays(numberOfGames: numberOfGames, numberOfTeams: numberOfTeams)
-        pairTeams()
+        self.teams.removeAll()
+
+        createTeams(numberOfTeams: numberOfTeams)
+        print(self.teams)
+        matchmaking(teams: self.teams)
+    }
+    
+    func createTeams(numberOfTeams: Int) {
+        for team in 1...numberOfTeams {
+            teams.append(team)
+        }
     }
     
     
-    func createArrays(numberOfGames: Int, numberOfTeams: Int) {
-        let minimumNumberOfGames = Int(numberOfGames / numberOfTeams)
-        let remainder = numberOfGames % numberOfTeams
+    func matchmaking(teams teamsLeftToPlay: [Int], teamsPlayed: [Int: [Int]] = [:]) {
         
-        for _ in 0..<minimumNumberOfGames {
-            for team in 1...numberOfTeams {
-                hostTeams.append(team)
+        guard numberOfGames > 0 else {
+            print("all games already played!")
+            return
+        }
+        
+        var teamsPlayed = teamsPlayed
+        
+        
+        for team in teamsLeftToPlay.shuffled() {
+            if let guest = teamsLeftToPlay.randomElement(),
+               !(teamsPlayed[team]?.contains(guest) ?? false),
+                team != guest,
+                numberOfGames > 0 {
+                print("\(team) vs \(guest)")
+               
+                numberOfGames -= 1
+                print("Number Of Games: \(numberOfGames)")
+                if let teamMatches = teamsPlayed[team], teamMatches.count > 0 {
+                    teamsPlayed[team]?.append(guest)
+                } else {
+                    teamsPlayed[team] = [guest]
+                }
             }
         }
         
-        if remainder > 0 {
-            for index in 1...remainder {
-                hostTeams.append(index)
-            }
-        }
+    matchmaking(teams: teamsLeftToPlay, teamsPlayed: teamsPlayed)
         
-        guestTeams = hostTeams.shift(withDistance: 1)
-        var tempArray = Array(guestTeams.prefix(upTo: numberOfTeams))
-        guestTeams = hostTeams.shift(withDistance: 2)
-        tempArray.append(contentsOf: Array(guestTeams.suffix(from: numberOfTeams)))
-        guestTeams = tempArray
-    }
-    
-    func pairTeams() {
-        
-        print(hostTeams)
-        print(guestTeams)
-        print("")
-        
-    }
-}
-
-
-
-extension Array {
-    
-    /**
-     Returns a new array with the first elements up to specified distance being shifted to the end of the collection. If the distance is negative, returns a new array with the last elements up to the specified absolute distance being shifted to the beginning of the collection.
-     
-     If the absolute distance exceeds the number of elements in the array, the elements are not shifted.
-     */
-    func shift(withDistance distance: Int = 1) -> Array<Element> {
-        let offsetIndex = distance >= 0 ?
-            self.index(startIndex, offsetBy: distance, limitedBy: endIndex) :
-            self.index(endIndex, offsetBy: distance, limitedBy: startIndex)
-        
-        guard let index = offsetIndex else { return self }
-        return Array(self[index ..< endIndex] + self[startIndex ..< index])
-    }
-    
-    /**
-     Shifts the first elements up to specified distance to the end of the array. If the distance is negative, shifts the last elements up to the specified absolute distance to the beginning of the array.
-     
-     If the absolute distance exceeds the number of elements in the array, the elements are not shifted.
-     */
-    mutating func shiftInPlace(withDistance distance: Int = 1) {
-        self = shift(withDistance: distance)
     }
     
 }
